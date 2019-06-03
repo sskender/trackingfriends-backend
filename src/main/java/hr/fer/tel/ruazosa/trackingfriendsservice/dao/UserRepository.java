d63a1f799d44cf099b24ae0ed31d73f8047a6323
sskender <sskender@users.noreply.github.com>
2019-06-04 00:15:21 +0200

    User repository mongo

---
+++ src/main/java/hr/fer/tel/ruazosa/trackingfriendsservice/dao/UserRepository.java
@@ -0,0 +1,31 @@
+package hr.fer.tel.ruazosa.trackingfriendsservice.dao;
+
+import hr.fer.tel.ruazosa.trackingfriendsservice.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    /**
+     * Check if email already exists in database for registration purposes.
+     *
+     * @param email user email
+     * @return User object
+     */
+    Optional<User> findByEmail(String email);
+
+    /**
+     * Validate user login with email and password.
+     *
+     * @param email    user email
+     * @param password user password
+     * @return User object
+     */
+    @Query("{ 'email' : ?0 , 'password' : ?1 }")
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+}
