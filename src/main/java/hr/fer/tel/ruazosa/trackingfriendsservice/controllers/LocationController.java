6c2d721e430f6860d6b140fe50a8167d7a7f5ef4
sskender <sskender@users.noreply.github.com>
2019-06-04 03:07:51 +0200

    Location controller

---
+++ src/main/java/hr/fer/tel/ruazosa/trackingfriendsservice/controllers/LocationController.java
@@ -0,0 +1,44 @@
+package hr.fer.tel.ruazosa.trackingfriendsservice.controllers;
+
+import hr.fer.tel.ruazosa.trackingfriendsservice.models.Location;
+import hr.fer.tel.ruazosa.trackingfriendsservice.services.ILocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(BaseApiUrl.BASE_API_URL + "/{userId}/location")
+public class LocationController {
+
+    private final ILocationService locationService;
+
+    @Autowired
+    public LocationController(ILocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping("/{friendId}")
+    public ResponseEntity<Location> getLocation(
+            @Valid @PathVariable String userId,
+            @Valid @PathVariable String friendId
+    ) {
+        Location location = locationService.getUserLocation(userId, friendId);
+
+        return new ResponseEntity<>(location, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> updateLocation(
+            @Valid @PathVariable String userId,
+            @Valid @RequestBody Location location
+    ) {
+        location.setUserId(userId);
+        Location updatedLocation = locationService.updateUserLocation(location);
+
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+}
