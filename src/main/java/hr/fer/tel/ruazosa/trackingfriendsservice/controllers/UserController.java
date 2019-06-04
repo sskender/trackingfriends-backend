1be7a43b2bc0b95a6b493b47cd7f19ed4ad3eb72
sskender <sskender@users.noreply.github.com>
2019-06-04 02:33:47 +0200

    User controller support to update user

--- src/main/java/hr/fer/tel/ruazosa/trackingfriendsservice/controllers/UserController.java
+++ src/main/java/hr/fer/tel/ruazosa/trackingfriendsservice/controllers/UserController.java
@@ -5,10 +5,7 @@ import hr.fer.tel.ruazosa.trackingfriendsservice.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -50,7 +47,26 @@ public class UserController {
 
     /* login */
 
+
+    /* update */
+
+    @PostMapping("/{userId}/update")
+    public ResponseEntity<Object> updateFields(
+            @Valid @PathVariable String userId,
+            @Valid @RequestBody User user
+    ) {
+        user.setUserId(userId);
+
+        userService.updateUser(user);
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    /* update */
+
+
     // TODO search users
     // TODO friends bla bla
+
 
 }
